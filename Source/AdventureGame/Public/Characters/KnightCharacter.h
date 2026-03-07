421d962ad4a035aa009b248eb15624b128ea01ac
Hercules <Dojhoo521@outlook.com>
2026-03-07 23:11:16 +0800

    Organize My Character Class.

--- Source/AdventureGame/Public/Characters/KnightCharacter.h
+++ Source/AdventureGame/Public/Characters/KnightCharacter.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -12,18 +10,17 @@ class ADVENTUREGAME_API AKnightCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	AKnightCharacter();
 
+	AKnightCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
 
 };
